e62af5759f8865313a0da16f2f0f16f83a6c09c6
LC-burigo <burigo-jm@hotmail.com>
2020-09-25 01:39:50 -0300

    Add the Average function

--- Clima/API_OpenWeatherMap.py
+++ Clima/API_OpenWeatherMap.py
@@ -24,7 +24,7 @@ class City_weather:
         data = response.json()
         return data
 
-    def Hourly_Temperature(self):
+    def Hourly_Features(self):
         call = City_weather.Weather_City(self)
         Hourly = call['hourly']
         Weather_Features = []
@@ -35,15 +35,15 @@ class City_weather:
         i = 0
         n = 0
         while i < len(Hourly):
-            Dict_Temperature['{}ª temperatura medida {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['temp']
-            Dict_Humidity['{}ª umidade medida {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['humidity']
-            Dict_WindSpeed['{}ª umidade medida {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['wind_speed']
-            Dict_Pressure['{}ª umidade medida {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['pressure']
+            Dict_Temperature['{}ª measured temperature {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['temp']
+            Dict_Humidity['{}ª measured humidity {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['humidity']
+            Dict_WindSpeed['{}ª measured wind speed {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['wind_speed']
+            Dict_Pressure['{}ª measured pressure {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['pressure']
             i = i + 1
             n = n + 1
         Weather_Features.append(Dict_Temperature, Dict_Humidity, Dict_WindSpeed, Dict_Pressure)
         print(Weather_Features)
-        return Dict_Temperature, Dict_Humidity, Dict_WindSpeed, Dict_Pressure
+        return Weather_Features
 
     def Average(self):
         call = City_weather.Weather_City(self)
@@ -72,6 +72,69 @@ class City_weather:
         print('The count of pressure measurements was {}'.format(i))
         print('the average pressure in the last {} hours is: {}'.format(i, Hour_Pressure / 24))
 
+    def Max(self):
+        call = City_weather.Weather_City(self)
+        Hourly = call['hourly']
+        Dict_Temperature = {}
+        Dict_Humidity = {}
+        Dict_WindSpeed = {}
+        Dict_Pressure = {}
+        i = 0
+        n = 0
+        while i < len(Hourly):
+            Dict_Temperature['{}ª measured temperature {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['temp']
+            Dict_Humidity['{}ª measured humidity {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['humidity']
+            Dict_WindSpeed['{}ª measured wind speed {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['wind_speed']
+            Dict_Pressure['{}ª measured pressure {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['pressure']
+            i = i + 1
+            n = n + 1
+        Max_Temperature = Dict_Temperature[max(Dict_Temperature, key=Dict_Temperature.get)]
+        Max_Humidity = Dict_Humidity[max(Dict_Humidity, key=Dict_Humidity.get)]
+        Max_WindSpeed = Dict_WindSpeed[max(Dict_WindSpeed, key=Dict_WindSpeed.get)]
+        Max_Pressure = Dict_Pressure[max(Dict_Pressure, key=Dict_Pressure.get)]
+
+        Max_Temperature_dt = max(Dict_Temperature, key=Dict_Temperature.get)
+        Max_Humidity_dt = max(Dict_Humidity, key=Dict_Humidity.get)
+        Max_WindSpeed_dt = max(Dict_WindSpeed, key=Dict_WindSpeed.get)
+        Max_Pressure_dt = max(Dict_Pressure, key=Dict_Pressure.get)
+
+        print('The maximum temperature was measured at {} with the value of {}'.format(Max_Temperature_dt, Max_Temperature))
+        print('The maximum Humidity was measured at {} with the value of {}'.format(Max_Humidity_dt, Max_Humidity))
+        print('The maximum WindSpeed was measured at {} with the value of {}'.format(Max_WindSpeed_dt, Max_WindSpeed))
+        print('The maximum Pressure was measured at {} with the value of {}'.format(Max_Pressure_dt, Max_Pressure))
+
+    def Min(self):
+        call = City_weather.Weather_City(self)
+        Hourly = call['hourly']
+        Dict_Temperature = {}
+        Dict_Humidity = {}
+        Dict_WindSpeed = {}
+        Dict_Pressure = {}
+        i = 0
+        n = 0
+        while i < len(Hourly):
+            Dict_Temperature['{}ª measured temperature {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['temp']
+            Dict_Humidity['{}ª measured humidity {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['humidity']
+            Dict_WindSpeed['{}ª measured wind speed {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['wind_speed']
+            Dict_Pressure['{}ª measured pressure {}'.format(n, Hourly[i]['dt'])] = Hourly[i]['pressure']
+            i = i + 1
+            n = n + 1
+        Min_Temperature = Dict_Temperature[min(Dict_Temperature, key=Dict_Temperature.get)]
+        Min_Humidity = Dict_Humidity[min(Dict_Humidity, key=Dict_Humidity.get)]
+        Min_WindSpeed = Dict_WindSpeed[min(Dict_WindSpeed, key=Dict_WindSpeed.get)]
+        Min_Pressure = Dict_Pressure[min(Dict_Pressure, key=Dict_Pressure.get)]
+
+        Min_Temperature_dt = min(Dict_Temperature, key=Dict_Temperature.get)
+        Min_Humidity_dt = min(Dict_Humidity, key=Dict_Humidity.get)
+        Min_WindSpeed_dt = min(Dict_WindSpeed, key=Dict_WindSpeed.get)
+        Min_Pressure_dt = min(Dict_Pressure, key=Dict_Pressure.get)
+
+        print('The minimum temperature was measured at {} with the value of {}'.format(Min_Temperature_dt, Min_Temperature))
+        print('The minimum Humidity was measured at {} with the value of {}'.format(Min_Humidity_dt, Min_Humidity))
+        print('The minimum WindSpeed was measured at {} with the value of {}'.format(Min_WindSpeed_dt, Min_WindSpeed))
+        print('The minimum Pressure was measured at {} with the value of {}'.format(Min_Pressure_dt, Min_WindSpeed))
+
 
 Florianopolis = City_weather(-27, -48, 1600898984)
-Florianopolis.Average()
+Florianopolis.Max()
+Florianopolis.Min()
--- Clima/OpenWeatherMap.py
+++ Clima/OpenWeatherMap.py
@@ -1,17 +1,19 @@
-import requests
-from pprint import pprint
+# import requests
+# from pprint import pprint
+#
+# city = input("Enter your city: ")
+# url = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=7904d58d2274ccc17d69331fbd3b9032'.format(city)
+# response = requests.get(url)
+# data = response.json()
+#
+# print('Cidade:', data['name'])
+# print('Vento:', data['wind']['speed'])
+# print('Temperatura:', data['main']['temp'])
+# print('Umidade:', data['main']['humidity'])
+# print(data)
 
-city = input("Enter your city: ")
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=7904d58d2274ccc17d69331fbd3b9032'.format(city)
-response = requests.get(url)
-data = response.json()
-
-print('Cidade:', data['name'])
-print('Vento:', data['wind']['speed'])
-print('Temperatura:', data['main']['temp'])
-print('Umidade:', data['main']['humidity'])
-print(data)
-
-
+dic = {'A': 58, 'B': 100, 'C': 42, 'D': 99, 'E': 12}
+print(dic[max(dic, key=dic.get)])
+print(max(dic, key=dic.get))
 
 
